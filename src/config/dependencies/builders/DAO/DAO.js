9ef33127e4a39e00c4812e08a08be01e2a8ac5e2
Rodrigo <rodrigojacques16@gmail.com>
2020-02-25 03:50:29 -0300

    foo

--- src/config/dependencies/builders/DAO/DAO.js
+++ src/config/dependencies/builders/DAO/DAO.js
@@ -3,14 +3,33 @@ module.exports = class DAO {
         this.db = db
         this.ObjectId = ObjectId
         this.collections = {
-            
+            chats: db.collection("chats"),
+            chat_types: db.collections("chat_types")
         }
     }
 
-    get_post(id) {
+    getChat(id) {
         return new Promise((resolve, reject) => {
             let { ObjectId } = this
-            this.collections.posts.find({ _id: ObjectId(id) }).toArray((erro, result) => {
+            this.collections.chats.find({ _id: ObjectId(id) }).toArray((erro, result) => {
+                if (erro) {
+                    reject(erro)
+                    return
+                }
+                if (result.length > 0) {
+                    resolve(result[0])
+                }
+                else {
+                    reject("That ID does not refer not any Chat")
+                }
+            })
+        });
+    }
+
+    getChatByUsers(users) {
+        return new Promise((resolve, reject) => {
+            let { ObjectId } = this
+            this.collections.posts.find({ users: users }).toArray((erro, result) => {
                 if (erro) {
                     reject(erro)
                     return
@@ -25,7 +44,7 @@ module.exports = class DAO {
         });
     }
 
-    registerPost(post) {
+    registerChat(Chat) {
         return new Promise(async (resolve, reject) => {
             try {
                 let insertionLog = await this.collections.posts.insertOne(post)
@@ -38,7 +57,15 @@ module.exports = class DAO {
         });
     }
 
-    delete_post(id) {
+    addChatMessage(){
+
+    }
+
+    updateChat(){
+
+    }
+
+    deleteChat(id) {
         return new Promise(async (resolve, reject) => {
             let { ObjectId } = this
 
@@ -52,46 +79,7 @@ module.exports = class DAO {
         });
     }
 
-    searchPosts(filters) {
-        return new Promise((resolve, reject) => {
-
-            //parsing data
-            let pagination = {
-                limit: filters.limit,
-                offset: filters.offset
-            }
-            delete filters.limit
-            delete filters.offset
-
-            if (filters.location) {
-                filters.location = { $elemMatch: filters.location }
-            }
-
-            if (filters.key_words) {
-                let key_words = filters.key_words
-                let keys = Object.keys(key_words)
-                delete filters.key_words
-        
-                for (let key of keys) {
-                    filters[`key_words.${key}`] = { $in: key_words[key] }
-                }
-                
-            }
-
-            if (filters.title) {
-                filters.title = new RegExp(".*" + filters.title + ".*")
-            }
-            
-            this.collections.posts.find(filters).limit(pagination.limit).skip(pagination.offset).toArray((erro, result) => {
-                if (erro) {
-                    return reject(erro)
-                }
-                resolve(result)
-            })
-        });
-    }
-
-    check_post(id) {
+    checkChat(id) {
         return new Promise((resolve, reject) => {
             let { ObjectId } = this
             this.collections.posts.find({ _id: ObjectId(id) }).project({ _id: 1 }).toArray((erro, result) => {
@@ -108,4 +96,23 @@ module.exports = class DAO {
             })
         });
     }
+
+    checkChatByUsers(id) {
+        return new Promise((resolve, reject) => {
+            let { ObjectId } = this
+            this.collections.posts.find({ _id: ObjectId(id) }).project({ _id: 1 }).toArray((erro, result) => {
+                if (erro) {
+                    reject(erro)
+                    return
+                }
+                if (result.length > 0) {
+                    resolve(true)
+                }
+                else {
+                    resolve(false)
+                }
+            })
+        });
+    }
+
 }
